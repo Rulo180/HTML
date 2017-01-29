3f28f3e15bbcca9e3174b48ace59db446def7d49
Rulo180 <rullencio180@gmail.com>
2017-01-29 18:42:25 -0300

    Agregado de Iconos y funcionalidades js

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
+
   var nameVar = "Martin";
-  console.log(nameVar);
 
   $('.pjs').text("el nombre del flaco es: " + nameVar);
 
@@ -10,13 +10,24 @@ $(document).ready(function () {
 //Activa o desactiva la clase "collapse" del navbar cuando se hace click en el boton
   $(".navbar-toggle").click(function() {
     //alert("showNav");
-    $('.navbar-nav').toggleClass( "collapse" );
+    $(".navbar-nav").toggleClass( "collapse" );
   });
 
-$('.dropdown-toggle').click(function (){
-  //alert("dropdown");
-  $('.dropdown').toggleClass('open');
-});
+  $(".dropdown-toggle").click(function (event){
+    event.preventDefault(); //Previene el comportamiento por defecto del browser vuelve la pagina al top por el href=#
+    $(".dropdown").toggleClass("open");
+  });
 
+  $(".offers-btn").on("click", '.btn-default', function () {
+    var amount = $(this).closest('article').data("price");  //Busca el ancenstro mas cercano del boton para pedirle el atributo data-price
+    var button = $("<button disabled>Desde $" + amount + " pesos.</button>");
+    button.addClass("btn btn-info text-capitalize");
+    $(this).parent().prepend(button);
+    $(this).remove();
+  });
+
+  $(".offers").find(".panel-heading").on("click", "span", function () {
+    $(this).closest("article").fadeOut();
+  });
 
 });
